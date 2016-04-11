073adfdc3fef393f6e9d4c3461d10a991c7d77f4
Emanuel Evans <mail@emanuel.industries>
2016-04-11 14:26:15 -0700

    Gem skeleton

---
+++ lib/pronto/commentator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+require 'pronto/commentator/version'
+
+module Pronto
+  module Commentator
+    # Your code goes here...
+  end
+end
---
+++ spec/pronto/commentator_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+describe Pronto::Commentator do
+  it 'has a version number' do
+    expect(Pronto::Commentator::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
